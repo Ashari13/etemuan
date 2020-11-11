5920fe7e781ae53b4e10f7f9ab70bb3dfd37b4d7
Ashari13 <asharigunawanhasan@gmail.com>
2020-11-11 14:14:30 +0700

    init

---
+++ beranda.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>E - TEMUAN Kabupaten Maros</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Logo -->
+  <link href="assets/img/gambar/Logo.png" rel="icon">
+  <link href="assets/img/gambar/Logo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto">
+        <a><img src="assets/img/gambar/Logo.png" alt="" class="img-fluid"></a>
+          <a href="beranda.php">E - Temuan</a></h1>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="beranda.php">Beranda</a></li>
+          <!-- <li class="drop-down"><a href="">Profil</a>
+            <ul>
+              <li><a href="#">Profil 1</a></li>
+              <li><a href="#">Profil 2</a></li>
+              <li><a href="#">Profil 3</a></li>
+              <li><a href="#">Profil 4</a></li>
+              <li class="drop-down"><a href="#">Profil Lainnya</a>
+                <ul>
+                  <li><a href="#">Profil Maros</a></li>
+                  <li><a href="#">Profil Maros</a></li>
+                  <li><a href="#">Profil Maros</a></li>
+                  <li><a href="#">Profil Maros</a></li>
+                  <li><a href="#">Profil Maros</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="#services">Informasi</a></li>
+          <li><a href="#portfolio">Berita</a></li>
+          <li><a href="#team">Galeri</a></li>
+          <li><a href="#contact">Kontak Kami</a></li> -->
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+        <a href="login" class="get-started-btn scrollto">Login</a>
+
+      </div>
+    </header>
+    <!-- End Header -->
+
+
+
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <img src="assets/img/gambar/galeri/latar_etemuan.png" class="img-fluid">
+
+      <div class="container">
+      <div> 
+        <img src="assets/img/hero-img.png" class="img-fluid animated" alt="">
+      </div>
+      </div>
+
+
+
+<!--        <div class="row">
+
+                <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+        
+                </div>
+            </div> -->
+
+
+
+  </section>  
+  <!-- End Hero -->
+
+
+
+  <main id="main">
+
+
+
+    <!-- Halaman Pencarian -->
+    <footer id="footer">
+
+      <div class="footer-newsletter">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-6">
+              <h4>Pencarian</h4>
+              <p>Carilah informasi-informasi yang berkaitan dengan E - Temuan</p>
+              <form action="" method="post">
+              <input type="text" name="cari" placeholder="Masukkan Pencarian Anda Disini"><input type="submit" value="Cari">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </footer>
+    <!-- Tutup halaman pencarian -->
+
+
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="row">
+          <div class="col-lg-9 text-center text-lg-left">
+            <h3>E - Temuan</h3>
+            <p>Merupakan Sebuah Sistem Pertemuan berbasis Website. Yang digunakan untuk melakukan pertemuan secara online kepada <b>Bagian Inspektorat.</b> </p>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="login">E - Temuan</a>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- End Cta Section -->
+
+
+
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Galeri</h2>
+          <p>Halaman galeri terbaru terkait E - Temuan kabupaten Maros.</p>
+        </div>
+
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-img"><img src="assets/img/gambar/galeri/galeri4.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>31 oct 2019</h4>
+              <a href="assets/img/gambar/galeri/galeri4.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="Card 3"><i class="bx bx-plus"></i></a> 
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-img"><img src="assets/img/gambar/galeri/galeri5.jpg" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>20 Oct 2019</h4>
+              <a href="assets/img/gambar/galeri/galeri5.jpg" data-gall="portfolioGallery" class="venobox preview-link" title="Card 3"><i class="bx bx-plus"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-img"><img src="assets/img/gambar/galeri/latar2.png" class="img-fluid" alt=""></div>
+            <div class="portfolio-info">
+              <h4>15 Nov 2020</h4>
+              <a href="assets/img/gambar/galeri/latar2.png" data-gall="portfolioGallery" class="venobox preview-link" title="Card 3"><i class="bx bx-plus"></i></a>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </section>  
+    <!-- End Portfolio Section -->
+
+
+  </main><!-- End #main -->
+
+
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+
+            <h3>E - Temuan</h3>
+            <p>
+              Jl. Asoka N0.5, Pettuadae <br>
+              Kecamatan Turikale, Kabupaten Maros<br>
+              Sulawesi Selatan 90516 <br><br>
+              <strong>Phone:</strong>(0411) 3881264<br>
+              <strong>Email:</strong> etemuanmaros@gmail.com<br>
+            </p>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; 2020 <strong><span>E - Temuan</span></strong>. Pemerintah Kabupaten Maros
+      </div>
+      <div class="credits">
+        Designed by <a href="#">KominfoKab.Maros</a>
+      </div>
+    </div>
+  </footer> 
+  <!-- End Footer -->
+
+
+
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
